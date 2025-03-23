fa6f93d22147441990cf3cb2411c4a0db6cc0b74
goingmyway243 <goingmyway243@gmail.com>
2025-03-23 23:43:38 +0700

    Update gitignore and environment.prod.ts

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    production: false,
-    baseUrl: 'http://localhost:5046',
+    production: true,
+    baseUrl: 'https://goingmysocialnetwork-api.azurewebsites.net',
     defaultAvatar: 'imgs/default-avatar.png',
-    chathubUrl: 'http://localhost:5046/chathub'
+    chathubUrl: 'https://goingmysocialnetwork-api.azurewebsites.net/chathub'
 }
