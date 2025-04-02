c9dc4497afca111b73d8fb393c2aed383fee882f
goingmyway243 <goingmyway243@gmail.com>
2025-04-02 23:45:04 +0700

    web: add loading when creating a post

--- web/src/app/dialogs/create-post-dialog/create-post-dialog.component.ts
+++ web/src/app/dialogs/create-post-dialog/create-post-dialog.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 import { UserAvatarComponent } from "../../components/user-avatar/user-avatar.component";
 import { MatDialogRef } from '@angular/material/dialog';
 import { AppCommonComponent } from '../../components/app-common/app-common.component';
 import { PostApiService } from '../../common/services/post-api.service';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../../common/services/auth.service';
-import { Content } from '../../common/models/content.model';
-import { ContentType } from '../../common/enums/content-type.enum';
 import { MatIconModule } from '@angular/material/icon';
+import { AppLoaderComponent } from "../../components/app-loader/app-loader.component";
 
 @Component({
   selector: 'create-post-dialog',
   standalone: true,
-  imports: [UserAvatarComponent, FormsModule, MatIconModule],
+  imports: [UserAvatarComponent, FormsModule, MatIconModule, AppLoaderComponent],
   templateUrl: './create-post-dialog.component.html',
   styleUrl: './create-post-dialog.component.scss'
 })
 export class CreatePostDialogComponent extends AppCommonComponent implements OnDestroy {
+  isLoading = signal(false);
+
   caption: string = '';
   uploadFiles: File[] = [];
   objectURLs: string[] = []
@@ -42,12 +43,19 @@ export class CreatePostDialogComponent extends AppCommonComponent implements OnD
   }
 
   createPost(): void {
+    if (!this.caption) {
+      return;
+    }
+
+    this.isLoading.set(true);
+
     const postRequest = {
       caption: this.caption,
       userId: this.authSvc.getCurrentUserId()
     };
 
     this.postApiSvc.createPost(postRequest, this.uploadFiles).subscribe(result => {
+      this.isLoading.set(false);
       this.dialogRef.close(result);
     });
   }
